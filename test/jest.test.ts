8feba7468ca7ba7df912938d2fe119e2631ed286
hakucherry <hakucherry@gmail.com>
2020-04-28 21:10:00 +0900

    update: circleci

--- test/jest.test.ts
+++ test/jest.test.ts
@@ -1,4 +1,11 @@
+import { add } from '../src/adder';
+
 // The assertion for a promise must be returned.
 it('always!', () => {
   expect(true).toBe(true);
+});
+
+// The assertion for a promise must be returned.
+it('add!', () => {
+  expect(add(2, 5)).toBe(7);
 });
