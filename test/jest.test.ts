d5f48dd84682f63a8e4aec41d3b31f90bfedbf74
hakucherry <hakucherry@gmail.com>
2020-04-28 23:32:29 +0900

    f

--- test/jest.test.ts
+++ test/jest.test.ts
@@ -3,10 +3,9 @@ import { add } from '../src/adder';
 const ws = new WebSocket('ws://localhost:8787');
 
 // The assertion for a promise must be returned.
-it('always!', () => {
+it('always!!', () => {
   const promise = new Promise((resolve: any, reject: any) => {
     ws.onmessage = (e: MessageEvent) => {
-      console.log(e.data);
       resolve(e.data);
     }
   }).then(response => {
