ca19cccd84059a8bca1a83b796585aecceeb256b
hakucherry <hakucherry@gmail.com>
2020-04-28 23:18:44 +0900

    a

--- test/jest.test.ts
+++ test/jest.test.ts
@@ -1,11 +1,24 @@
 import { add } from '../src/adder';
 
+const ws = new WebSocket('ws://localhost:8787');
+
 // The assertion for a promise must be returned.
 it('always!', () => {
-  expect(true).toBe(true);
+  const promise = new Promise((resolve: any, reject: any) => {
+    ws.onmessage = (e: MessageEvent) => {
+      console.log(e.data);
+      resolve(e.data);
+    }
+  }).then(response => {
+    expect(response).toEqual('hello world!');
+  })
+  ws.onopen = () => {
+    ws.send('hello world!');
+  }
+  return promise;
 });
 
 // The assertion for a promise must be returned.
 it('add!', () => {
-  expect(add(2, 5)).toBe(6);
+  expect(add(2, 5)).toBe(3);
 });
